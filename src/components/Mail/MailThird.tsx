6ba2c24aaba695eef715323dc78a9bd294fed7d6
naerim <naerim1119@gmail.com>
2022-11-12 23:10:38 +0900

    [FEAT] 메일 - 파일 업로드 배경

--- src/components/Mail/MailThird.tsx
+++ src/components/Mail/MailThird.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import MailLayout from './MailLayout';
+const Mail1 = process.env.PUBLIC_URL + '/img/mail1.png';
 
 type MailThirdType = {
   stage: number;
@@ -16,12 +17,40 @@ const MailThird: React.FC<MailThirdType> = ({ stage, setStage }) => {
         title={'홍길동님께' + '\n' + '편지를 보내볼까요?'}
         desc="*mp4형식의 동영상 파일만 업로드 가능해요."
       >
-        <Container>Third</Container>
+        <Wrap>
+          <img src={Mail1} alt="" />
+          <div className="receiver">홍길동</div>
+        </Wrap>
       </MailLayout>
     </>
   );
 };
-
-const Container = styled.div``;
+const Wrap = styled.div`
+  display: flex;
+  img {
+    width: 630px;
+    height: 403px;
+    object-fit: cover;
+  }
+  .receiver {
+    position: absolute;
+    left: 195px;
+    top: 50px;
+    width: 158px;
+    height: 38px;
+    background: #ffffff;
+    box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.1);
+    border-radius: 8px;
+    display: flex;
+    align-items: center;
+    padding: 0 10px;
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 20px;
+    color: #1c1c1c;
+  }
+`;
 
 export default MailThird;
