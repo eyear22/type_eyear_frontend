796bb4f9cc0a17a743979241638e33ade5a951f8
naerim <naerim1119@gmail.com>
2023-04-06 23:47:14 +0900

    [FEAT] 타이틀 추가

--- src/components/Main/MainTop.tsx
+++ src/components/Main/MainTop.tsx
@@ -4,7 +4,10 @@ const MainTop = () => {
   return (
     <>
       <Container>
-        <div>img</div>
+        <TitleWrap>
+          <div>진심을 전하는</div>
+          <div>가장 쉬운 방법</div>
+        </TitleWrap>
       </Container>
     </>
   );
@@ -13,6 +16,24 @@ const MainTop = () => {
 const Container = styled.div`
   height: 810px;
   background: #edf6f1;
+  padding-top: 110px;
+`;
+
+const TitleWrap = styled.section`
+  margin-top: 72px;
+  width: 100%;
+  align-items: center;
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 32px;
+  letter-spacing: -1px;
+  color: #51cd89;
+  div {
+    display: flex;
+    justify-content: center;
+  }
 `;
 
 export default MainTop;
